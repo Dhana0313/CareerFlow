4cce666af3f24e69d742a9da73ad2a44a90d67ba
Dhananjaya Jayaweera <dananjayajayaweera1262@gmail.com>
2026-02-07 12:16:07 +0530

    feat: create UserDetailsService to load users from DB

--- src/main/java/com/dhananjaya/Job/Portal/repository/UserRepository.java
+++ src/main/java/com/dhananjaya/Job/Portal/repository/UserRepository.java
@@ -5,7 +5,9 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.dhananjaya.Job.Portal.model.User;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
     Boolean existsByEmail(String email);
